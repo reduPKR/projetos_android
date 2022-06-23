ef89ded577551c5c5e45c88c6f9273cfb2db1a2b
Rafael Eduardo <56879793+reduPKR@users.noreply.github.com>
2022-06-23 08:54:54 -0300

    Curso web api com andoir

---
+++ android-com-kotlin-comunicacao-web-aula-5/app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
@@ -0,0 +1,52 @@
+package br.com.alura.ceep.webclient
+
+import android.util.Log
+import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.webclient.model.NotaRequisicao
+import br.com.alura.ceep.webclient.services.NotaService
+
+private const val TAG = "NotaWebClient"
+
+class NotaWebClient {
+
+    private val notaService: NotaService =
+        RetrofitInicializador().notaService
+
+    suspend fun buscaTodas(): List<Nota>? {
+        return try {
+            val notasResposta = notaService
+                .buscaTodas()
+            notasResposta.map { notaResposta ->
+                notaResposta.nota
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "buscaTodas: ", e)
+            null
+        }
+    }
+
+    suspend fun salva(nota: Nota): Boolean {
+        try {
+            val resposta = notaService.salva(nota.id, NotaRequisicao(
+                titulo = nota.titulo,
+                descricao = nota.descricao,
+                imagem = nota.imagem
+            ))
+            return resposta.isSuccessful
+        } catch (e: Exception) {
+            Log.e(TAG, "salva: falha ao tentar salvar", e)
+        }
+        return false
+    }
+
+    suspend fun remove(id: String): Boolean {
+        try {
+            notaService.remove(id)
+            return true
+        } catch (e: Exception) {
+            Log.e(TAG, "remove: falha ao tentar remover nota", e)
+        }
+        return false
+    }
+
+}
