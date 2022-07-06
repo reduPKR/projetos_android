5ec343831187d2096536db4d766d8a685e498436
Rafael Eduardo <56879793+reduPKR@users.noreply.github.com>
2022-07-06 20:39:05 -0300

    projetos tech news

--- agenda/app/src/main/java/br/com/agenda/ui/activity/FormularioAlunoActivity.java
+++ agenda/app/src/main/java/br/com/agenda/ui/activity/FormularioAlunoActivity.java
@@ -5,6 +5,8 @@ import static br.com.agenda.ui.activity.Contantes.CHAVE_ALUNO;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -96,4 +98,22 @@ public class FormularioAlunoActivity extends AppCompatActivity {
 
         finish();
     }
+
+    //Rotaçãp
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater()
+                .inflate(R.menu.activity_formulario_aluno_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.activity_formulario_aluno_menu_salvar){
+            finalizar();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
